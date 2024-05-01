1b0c23e928275fe5af118da7fa31f96bd356c67e
Lav <killerboylav4904@gmail.com>
2024-05-01 22:22:56 +0530

    Major changes/functions updated

--- app.js
+++ app.js
@@ -1,56 +1,52 @@
-let container =  document.querySelector(".container");
-let rows = document.getElementsByClassName("gridRow");
+let container = document.querySelector(".container");
+let rows = document.getElementsByClassName("row");
 let cells = document.getElementsByClassName("cell");
 
-
+//Starts the game by clicking button.
 let startBtn = document.querySelector(".start-btn");
 startBtn.addEventListener("click", startGame);
 
+//Starts the game.
 function startGame() {
-    // deleteGrids();
-    let userNum = prompt("Enter the no. :");
-    if(userNum <= 100 && userNum > 0) {
-        createGrid(userNum);
-        console.log(userNum);
-    }
+  deleteGrid();
+  let userNum = prompt("Enter the number (1-100) :");
+  if (userNum <= 100 && userNum > 0) {
+    createGrid(userNum);
+  }
 }
 
+//Creates the no. of rows inside the container.
 function makeRows(rowNum) {
-    for(let i=0; i < rowNum; i++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    }
+  for (let i = 0; i < rowNum; i++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "row";
+  }
 }
 
-function makeColumns(columnNum) {
+//Creates the no. of cells inside the rows.
+function makeColumns(cellNum) {
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < cellNum; j++) {
+      let cell = document.createElement("div");
+      rows[j].appendChild(cell).className = "cell";
 
-    for(let i=0; i < rows.length; i++) {
-        for(let j = 0; j < columnNum; j++) {
-            let cell = document.createElement("div");
-            rows[j].appendChild(cell).className = "cell";
-
-            cell.addEventListener("mouseenter", () => {
-                cell.style.backgroundColor = "white";
-
-            })
-
-        } 
+      cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "#592E83";
+      });
+      cell.addEventListener("click", () => {
+        cell.style.backgroundColor = "white";
+      });
     }
-   }
+  }
+}
 
-
+//Creates the grid.
 function createGrid(num) {
-    makeRows(num);
-    makeColumns(num);
-    
+  makeRows(num);
+  makeColumns(num);
 }
 
-
-function deleteGrids() {
-    let row = document.querySelectorAll(".gridRow");
-    let cell = document.querySelectorAll(".cell");
-    row.removeChild(cell);
-    container.removeChild(row);
-
+//Deletes the previous grid.
+function deleteGrid() {
+  container.replaceChildren();
 }
-
